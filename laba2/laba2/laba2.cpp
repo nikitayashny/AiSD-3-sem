beada145297834e6525723c52b2b3d52cc16a02d
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-03-02 12:50:32 +0300

    Add files via upload

---
+++ laba2/laba2/laba2.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list> 
+#include <algorithm> 
+
+using namespace std;
+int dfs(int** graph, const int n, int from, const int to, int* visited, int* path);
+bool bfs(int** graph, int n, int to, int* visited, list<int>& buffer, int* parents); 
+void print_path(int from, int to, int* parents, ofstream& ofst); 
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    char filenamein[] = "in.txt";
+    char filenameout[] = "out.txt";
+
+    ifstream fin(filenamein);
+    ofstream fout(filenameout);
+    if (!fin || !fout) {
+        cout << "ошибка при открытие файла\n";
+        system("pause");
+        return 1;
+    }
+
+    int n, m;
+    fin >> n >> m;
+    int from, to;
+    fin >> from >> to;
+    from--; to--;
+
+    int** graph = new int* [n];
+    int* visited = new int[n];
+    int* path = new int[n];
+    int* parents = new int[n]; 
+
+    for (int i = 0; i < n; i++) {
+        graph[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            graph[i][j] = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        fin >> a >> b;
+        graph[a - 1][b - 1] = graph[b - 1][a - 1] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    int choose;
+    cout << "Выберите вид поиска: 1 - в глубину, 2 - в ширину." << endl;
+    cin >> choose;
+
+    int path_size;
+
+    list<int> buffer;
+    buffer.push_back(from);
+
+    switch (choose)
+    {
+    case(1):
+        path_size = dfs(graph, n, from, to, visited, path);
+        if (path_size < 0) {
+            fout << "Путь не существует";
+        }
+        else {
+            for (int i = path_size; i >= 0; --i) {
+                fout << path[i] + 1;
+                if (i == 0)
+                    fout << endl;
+                else
+                    fout << " -> ";
+            }
+        }
+        break;
+    case(2):
+        if (false == bfs(graph, n, to, visited, buffer, parents)) {
+            fout << "Путь не существует" << endl;
+        }
+        else {
+            print_path(from, to, parents, fout);
+        }
+        break;
+    default:
+        break;
+    }
+
+    for (int i = 0; i < n; i++)
+        delete[] graph[i];
+
+    delete[] graph;
+    delete[] visited;
+    delete[] path;
+    delete[] parents;
+
+    fin.close();
+    fout.close();
+}
+
+int dfs(int** graph, const int n, int from, const int to, int* visited, int* path) {
+    if (visited[from] == true)
+        return -1;
+    visited[from] = true;
+
+    if (from == to) {
+        path[0] = to;
+        return 0;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (graph[from][i] == 0)
+            continue;
+        int path_size = dfs(graph, n, i, to, visited, path);
+        if (path_size < 0)
+            continue;
+        path[path_size + 1] = from;
+        return path_size + 1;
+    }
+    return -1;
+}
+
+bool bfs(int **graph, int n, int to, int* visited, list<int>& buffer, int* parents) {
+    if (buffer.empty())
+        return false;
+    int from = buffer.front();
+    buffer.pop_front();
+    visited[from] = true;
+    if (from == to) {
+        return true;
+    }
+    for (int i = 0; i < n; ++i) {
+        if (graph[from][i] == 0)
+            continue;
+        if (visited[i] == true)
+            continue;
+        if (find(buffer.begin(), buffer.end(), i) != buffer.end())
+            continue;
+        parents[i] = from;
+        buffer.push_back(i);
+    }
+    return bfs(graph, n, to, visited, buffer, parents);
+}
+void print_path(int from, int to, int* parents, ofstream& ofst) {
+    if (to == from) {
+        ofst << to+1;
+        return;
+    }
+    int prefrom = parents[to];
+    print_path(from, prefrom, parents, ofst);
+    ofst << " -> " << to+1;
+}
---
+++ laba3/laba3/laba3.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{   
+    setlocale(LC_ALL, "RUS");
+    vector<int> parent(7, -1);         
+    int big_num(1000000);
+    int matrix[7][7] = { {0,  2,  5, 4,  0,  2,  0},      //A - 1, B - 2, C - 3, D - 4, E - 5, F - 6, G - 7, H - 8, I - 9
+                         {4,  0,  0,  1,  0,  4,  1},
+                         {2, 1,  0,  1,  2, 1,  1},
+                         {0,  3,  0,  0,  0, 3,  2},
+                         {0,  1,  0, 0, 0,  3,  4},
+                         {3,  0,  0,  3,  0,  0,  1},
+                         {5,  0, 2,  1,  0,  2,  0} };
+
+    int pos[7], node[7], min(0), index_min(0);
+    for (int i = 0; i < 7; ++i) {     
+        pos[i] = big_num;             // заполняем путь большими числами
+        node[i] = 0;                  // веришины обозначаем как непройденные
+    }
+
+    cout << "Введите стартовую вершину: ";  // назначаем вершину начала
+    int start; cin >> start;
+    if (start > 7 || start < 1) {
+        cout << "Такая вершина в графе отсутствует" << endl;
+        return 0;
+    }
+    pos[start-1] = 0;                 
+
+    for (int i = 0; i < 6; ++i) {     // основной цикл
+        min = big_num;
+        for (int j = 0; j < 7; ++j) { // находим вершину с минимальным к ней расстоянием
+            if (!node[j] && pos[j] < min)
+            {
+                min = pos[j];
+                index_min = j;
+            }
+        }
+        node[index_min] = true;       // помечаем выбранную вершину как пройденную
+        for (int j = 0; j < 7; ++j) { // даём всем вершинам смежным с выбранной путь к ней
+            if (!node[j] && matrix[index_min][j] > 0 && pos[index_min] != big_num && pos[index_min] + matrix[index_min][j] < pos[j])
+            {
+                pos[j] = pos[index_min] + matrix[index_min][j];
+                parent.at(j) = index_min;  // запоминаем предка вершины j
+            }
+            // если эта вершина не пройденная и она смежна с выбранной и если сумма веса выбранной вершины и ребра к текущей будет меньше, чем
+            // вес текущей на данный момент, то  - меняем значение веса текущей вершины.
+        }
+    }
+    for (int n = 1; n <= 7; n++)
+    {
+        vector<int>temp;
+        for (int i = n - 1; i != -1; i = parent.at(i))temp.push_back(i + 1);    // каждый раз, когда из выбранной вершины v происходит улучшение
+        reverse(temp.begin(), temp.end());                                      // расстояния до некоторой вершины to, мы записываем, что предком
+        cout << endl << "Путь: " << temp.at(0);                                 // вершины to является вершина v    p[to}=v
+        for (int i = 1; i < temp.size(); ++i)cout << " -> " << temp.at(i);
+
+        cout << endl << "Вес: " << pos[n - 1] << endl;                          // пройденное расстояние
+    }
+}
