3932200f92bb3a22d1c36e879b7390845f4bdf79
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-03-02 12:46:00 +0300

    Add files via upload

---
+++ laba1/laba1/laba1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void hanoi(int i, int k, int n)
+{
+    if (n == 1) {
+        cout << "Передвинуть диск 1 со стержня " << i << " на стержень " << k << ".\n";
+    }
+    else {
+        int tmp = 6 - i - k; 			
+        hanoi(i, tmp, n - 1);
+        cout << "Передвинуть диск " << n << " со стержня " << i << " на стержень " << k << ".\n";
+        hanoi(tmp, k, n - 1);
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n,k;
+
+    cout << "Введите число дисков: \n";
+    cin >> n;
+    cout << "Введите стержень: \n";
+    cin >> k;
+
+    hanoi(1, k, n);
+}
